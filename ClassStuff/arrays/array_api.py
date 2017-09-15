251f4394eec15467d7a304b4d0963b58aefe605d
Chris Palmer <christian_palmer@byu.edu>
2017-09-15 13:12:22 -0600

    made the final changes and now it should work. Still needs error messages

--- ClassStuff/arrays/array_api.py
+++ ClassStuff/arrays/array_api.py
@@ -20,7 +20,7 @@ class Array(object):
         
     def _check_bounds(self, index):
         '''Ensures the index is within the bounds of the array: 0 <= index <= size.'''
-        if index > self.size or index < 0:
+        if index >= self.size:
             return False
         else:
             return True
@@ -45,7 +45,13 @@ class Array(object):
         If a decrease is warranted, it should be done by allocating a new array and copying the
         data into it (don't allocate multiple arrays if multiple chunks need decreasing).
         '''
-            
+        if self.data.count(None) >= 5:
+            newArray = alloc(self.size)
+            memcpy(newArray, self.data, self.size)
+            self.data = newArray
+            return True
+        else:
+            return False 
         
     def add(self, item):
         '''Adds an item to the end of the array, allocating a larger array if necessary.'''
@@ -60,8 +66,21 @@ class Array(object):
         '''Inserts an item at the given index, shifting remaining items right and allocating a larger array if necessary.'''
         try:
             if(self._check_increase()):
+                i = self.size
+                for r in range(self.size - index):
+                    self.data[i] = self.data[i-1]
+                    self.data[i-1] = None
+                    i -= 1
                 self.data[index] = item
-            
+                self.size +=1
+            else:
+                i = self.size
+                for r in range(self.size - index):
+                    self.data[i] = self.data[i-1]
+                    self.data[i-1] = None
+                    i -= 1
+                self.data[index] = item
+                self.size +=1   
         except:
             print('Did not work')
     
@@ -83,14 +102,18 @@ class Array(object):
     def delete(self, index):
         '''Deletes the item at the given index, decreasing the allocated memory if needed.  Throws an exception if the index is not within the bounds of the array.'''
         if (self._check_bounds(index)):
+            for r in range(self.size - (index + 1)):
+                self.data[index] = self.data[index+1]
+                index += 1
             self.data[index] = None
             self.size -= 1
-        
+            self._check_decrease()
     def swap(self, index1, index2):
         '''Swaps the values at the given indices.'''
-        x = self.data[index1]
-        self.data[index1] = self.data[index2]
-        self.data[index2] = x
+        if (self._check_bounds(index2)):
+            x = self.data[index1]
+            self.data[index1] = self.data[index2]
+            self.data[index2] = x
         
         
         
@@ -111,9 +134,18 @@ def memcpy(dest, source, size):
     '''
     Copies items from one array to another.  This is similar to C's memcpy function.
     '''
-    for x in source:
-        for y in range(len(source)):
+    if len(dest) > len(source):
+        y = 0
+        for x in source:
             dest[y] = x
-    return dest
+            y += 1
+        return dest
+    else:
+        y = 0
+        for x in range(len(source) - 5):
+            dest[y] = source[x]
+            y += 1
+        return dest
+
 
         
--- ClassStuff/arrays/main.py
+++ ClassStuff/arrays/main.py
@@ -1,22 +1,66 @@
 from array_api import Array, memcpy
 import csv
 
-# with open('data_example.csv') as csvfile:
-#     reader = csv.reader(csvfile, delimiter=',')
-#     for row in reader:
-#         command = str.lower(row[0])
-#         element = row[1]
-        # print(command, element)
-array1 = Array()
-array1.add('test0')
-array1.add('test1')
-array1.add('test2')
-array1.add('test0')
-array1.add('test1')
-array1.add('test2')
-array1.add('test0')
-array1.add('test1')
-array1.add('test0')
-array1.add('test1')
-array1.debug_print()
+with open('data.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        command = row[0]
+        if command == 'CREATE':
+                array1 = Array()
+        elif command == 'DEBUG':
+                array1.debug_print()
+        elif command == 'ADD':
+                array1.add(row[1])
+        elif command == 'GET':
+                array1.get(int(row[1]))
+        elif command == 'SET':
+                array1.set(int(row[1]),row[2])
+        elif command == 'INSERT':
+                array1.insert(int(row[1]),row[2])
+        elif command == 'DELETE':
+                array1.delete(int(row[1]))
+        elif command == 'SWAP':
+                array1.swap(int(row[1]), int(row[2]))
+
+# array = Array()
+# array.debug_print()
+# array.add('a')
+# array.add('e')
+# array.add('r')
+# array.add('o')
+# array.add('I')
+# array.add('o')
+# array.add('d')
+# array.add('u')
+# array.add('s')
+# array.add('a')
+# array.debug_print()
+# array.add('u')
+# array.debug_print()
+# array.set(4, 'S')
+# array.set(12, 'M')
+# array.debug_print()
+# array.get(8)
+# array.get(11)
+# array.insert(21, 'b')
+# array.insert(4, 'i')
+# array.insert(4, 'L')
+# array.insert(4, 'w')
+# array.insert(4, 'x')
+# array.insert(4, 'T')
+# array.insert(4, 'y')
+# array.debug_print()
+# array.delete(6)
+# array.debug_print()
+# array.delete(10)
+# array.debug_print()
+# array.delete(7)
+# array.debug_print()
+# array.delete(13)
+# array.debug_print()
+# array.swap(1,3)
+# array.swap(7,8)
+# array.debug_print()
+
+
 
