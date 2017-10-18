5a397784098268c11c6a63bf9897ce66c4414129
Chris Palmer <christian_palmer@byu.edu>
2017-10-18 11:13:40 -0600

    Start sort analytics on new branch

---
+++ Projects/sortanalytics/sort_api.py
@@ -0,0 +1,42 @@
+class SortMethods(object):
+
+    def bubble_sort(self, input):
+        length = len(input)
+        for i in range(length-1, -1, -1):
+            for j in range(i):
+                if input[j] > input[j+1]:
+                    input[j], input[j+1] = input[j+1], input[j]
+
+        return input
+
+    def insertion_sort(self, input):
+        l = len(input)
+        for i in range(1, l):
+            while 0 < i and input[i] < input[i-1]:
+                input[i], input[i-1] = input[i-1], input[i]
+                i -= 1
+        return input
+
+    def select_sort(self, input):
+        l = len(input)
+        for i in range(l):
+            small = i
+            for x in range(i+1, l):
+                if input[x] < input[small]:
+                    small = x
+            input[small], input[i] = input[i], input[small]
+        return input
+
+    def quick_sort(self, input):
+        ARRAY_LENGTH = len(input)
+        if( ARRAY_LENGTH <= 1):
+            return input
+        else:
+            PIVOT = input[0]
+            # GREATER = [ element for element in input[1:] if element > PIVOT ]
+            GREATER = []
+            for element in input[1:]:
+                if element > PIVOT:
+                    GREATER.append(element)
+            LESSER = [ element for element in input[1:] if element <= PIVOT ]
+            return quickSort(LESSER) + [PIVOT] + quickSort(GREATER)
